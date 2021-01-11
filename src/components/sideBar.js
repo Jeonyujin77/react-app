7c3f457cf284a3903787fc9a5b50182730706351
Yujin Jeon <wjsdbwls7777@gmail.com>
2021-01-11 11:23:09 +0900

    create 구현: shouldComponentUpdate

--- src/components/sideBar.js
+++ src/components/sideBar.js
@@ -1,33 +1,39 @@
 import React,{ Component } from 'react';
 
 class SideBar extends Component {
-    render() {
-      var lists = []
-      var data = this.props.data
-      var i = 0;
-      while (i < data.length) {
-        lists.push(
-          <li key={data[i].id}>
-            <a 
-              href={"/content/"+data[i].id}
-              // data-id={data[i].id}
-              onClick={function(id, e) {
-                e.preventDefault();
-                // this.props.onChangePage(e.target.dataset.id);
-                this.props.onChangePage(id);
-              }.bind(this, data[i].id)}
-              > {data[i].title} </a>
-          </li>)
-        i++;
-      }
-      return (
-        <nav>
-          <ul>
-              {lists}
-          </ul>
-      </nav>
-      );
+  shouldComponentUpdate(newProps, newState) {
+    if(this.props.data === newProps.data) {
+      return false;
     }
+    return true
   }
+  render() {
+    var lists = []
+    var data = this.props.data
+    var i = 0;
+    while (i < data.length) {
+      lists.push(
+        <li key={data[i].id}>
+          <a 
+            href={"/content/"+data[i].id}
+            // data-id={data[i].id}
+            onClick={function(id, e) {
+              e.preventDefault();
+              // this.props.onChangePage(e.target.dataset.id);
+              this.props.onChangePage(id);
+            }.bind(this, data[i].id)}
+            > {data[i].title} </a>
+        </li>)
+      i++;
+    }
+    return (
+      <nav>
+        <ul>
+            {lists}
+        </ul>
+    </nav>
+    );
+  }
+}
 
-  export default SideBar;
+export default SideBar;
